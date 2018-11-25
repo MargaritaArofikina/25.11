7bae4f30d90223926f743500af483b0aa22385cc
MargaritaArofikina <44144804+MargaritaArofikina@users.noreply.github.com>
2018-11-25 21:04:54 +0300

    Update 9.cpp

--- 9.cpp
+++ 9.cpp
@@ -12,17 +12,17 @@ int main(){
 
 	bool* used = new bool[n + 1];
 
-	for (int i = 2; i <= n*2 + 1; i++){
+	for (int i = 2; i <= n * 2 + 1; i++){
 		*(used + i) = true;
 	}
-	for (int i = 2; i <= n*2 + 1; i++){
+	for (int i = 2; i <= n * 2 + 1; i++){
 		if (*(used + i) != false){
 			for (int j = i + i; j <= n; j += i){
 				*(used + j) = false;
 			}
 		}
 		if (*(used + i) == true) {
-			if (*(used + 2 * i + 1) == true) cout << i << endl;
+			if (*(used + 2 * i + 1) == true) cout << i << " " << 2 * i + 1 << endl;
 		}
 	}
 
