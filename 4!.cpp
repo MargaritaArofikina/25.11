e15511dd2db622bc81e752a4fd218845453c0093
MargaritaArofikina <44144804+MargaritaArofikina@users.noreply.github.com>
2018-11-25 16:44:45 +0300

    Update 4!.cpp

--- 4!.cpp
+++ 4!.cpp
@@ -3,36 +3,31 @@
 #include <iostream>
 using namespace std;
 
-int n;
-
-bool* used = new bool[n + 1];
-
-	for (int i = 0; i <= n; i++){
-		*(used + i) = i;
-	}
-
-	*used[0] = *used[1] = false;
-
-	for (int i = 2; i <= n; i++){
-		*used[i] = true;
-	}
-	for (int i = 2; i <= n; i++){
-		if (*used[i] != false){
-			for (int j = i + i; j <= n; j += i){
-				*used[j] = false;
-			}
-		}
-	}
-
 
 int main(){
 
 	int n;
 	cin >> n;
 
-	for (int i = 0; i <= n; i++){
+	bool* used = new bool[n + 1];
 
-		if (*(used + i) == false) cout << *(used + i) << endl;
+	for (int i = 2; i <= n; i++){
+		*(used + i) = true;
+	}
+	for (int i = 2; i <= n; i++){
+		if (*(used + i) != false){
+			for (int j = i + i; j <= n; j += i){
+				*(used + j) = false;
+			}
+		}
+	}
+
+
+
+
+	for (int i = 2; i <= n; i++){
+
+		if (*(used + i) == false) cout << i << endl;
 
 	}
 
