c679a0294315fe3b302587efe049480ccaebc20d
MargaritaArofikina <44144804+MargaritaArofikina@users.noreply.github.com>
2018-11-25 18:45:11 +0300

    Create 9.cpp

---
+++ 9.cpp
@@ -0,0 +1,30 @@
+//Напишите программу, находящую все простые числа Софи Жермен, не превосходящие n.
+
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+
+	int n;
+	cin >> n;
+
+	bool* used = new bool[n + 1];
+
+	for (int i = 2; i <= n*2 + 1; i++){
+		*(used + i) = true;
+	}
+	for (int i = 2; i <= n*2 + 1; i++){
+		if (*(used + i) != false){
+			for (int j = i + i; j <= n; j += i){
+				*(used + j) = false;
+			}
+		}
+		if (*(used + i) == true) {
+			if (*(used + 2 * i + 1) == true) cout << i << endl;
+		}
+	}
+
+	return 0;
+}
