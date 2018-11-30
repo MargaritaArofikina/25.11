a2a6776b2080160d1f7d322b64457a1ebc23a24e
MargaritaArofikina <44144804+MargaritaArofikina@users.noreply.github.com>
2018-11-30 21:46:37 +0300

    Update 5!.cpp

--- 5!.cpp
+++ 5!.cpp
@@ -4,64 +4,38 @@
 using namespace std;
 
 
-int amount(int n, int k, int* arr){
+void massive(int* arr, int n, int k){
 
 	int m = 1;
-	int s = 0;
-	for (int i = 0; i < n; i++){
-		if (*(arr + i) != 0) {
-			for (int j = 1; j < *(arr + i); j *= 10){
-				if ((*(arr + i) % (10 * j)) / j != 0) m *= (*(arr + i) % (10 * j));
-			}
-			if (m > k) s++;
-		}
-	}
-	return s;
-}
-
-void massive (int n, int k, int* arr){
-
-	int m = 1;
-	int l = amount(n, k, arr);
-	int *arr1 = new int[l];
-
 
 	for (int i = 0; i < n; i++){
-		if (*(arr + i) != 0) {
-			for (int j = 1; j < *(arr + i); j *= 10){
-				if ((*(arr + i) % (10 * j)) / j != 0) m *= *(arr + i);
-			}
+		for (int j = 1; *(arr + i) >= 1; j++){
+			if (*(arr + i) % 10 != 0) m *= (*(arr + i) % 10);
+
 			if (m > k) {
-				for (int p = 0; p < l; p++){
-					*(arr1 + p) = *(arr + i);
-				}
+				cout << *(arr + i) << endl;
+				break;
 			}
+			*(arr + i) /= 10;
 		}
+		
 	}
 
-	arr = arr1;
-
 }
-
 
 int main(){
 
-	int n, k, l;
+	int n, k;
 	cin >> n >> k;
-	
+
 	int* arr = new int[n];
 
 	for (int i = 0; i < n; i++){
 		cin >> *(arr + i);
 	}
 
-	massive(n, k, arr);
-
-	l = amount(n, k, arr);
-
-	for (int i = 0; i < l; i++){
-		cout << *(arr + i) << endl;
-	}
+	cout << endl;
+	massive(arr,n,k);
 
 	return 0;
 }
